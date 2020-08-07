65ed6c0c859c8e5c4e6e97c5d0819a2d107e05e9
gggonzalez <ggonzalez@glamit.com.ar>
2020-08-06 22:12:01 -0300

    commit0608

--- papasemuere/js/functions.js
+++ papasemuere/js/functions.js
@@ -3,7 +3,59 @@ $(window).scroll(function(){
     var wScroll = $(this).scrollTop();
 
     $('.logo').css({
-        'transfrom' : 'translate(0px, '+ wScroll /2+'%)'
-    });
+        'transform' : 'translate(0px, '+ wScroll /2+'%)'
+    })
+
+    if(wScroll > $('.diapos').offset().top - 
+    ($(window).height() / 1.2)){
+
+       
+
+        $('.diapos figure').each(function(i){
+
+            setTimeout(function(){
+
+            
+            $('.diapos figure').eq(i).addClass('is-showing');
+        }, 150 * (i+1));
+
+        });
+    }
+
+    if(wScroll > $('.diapos2').offset().top - 
+    ($(window).height() / 1.2)){
+
+       
+
+        $('.diapos2 figure').each(function(i){
+
+            setTimeout(function(){
+
+            
+            $('.diapos2 figure').eq(i).addClass('is-showing');
+        }, 150 * (i+1));
+
+        });
+    }
+
+    if(wScroll > $('.diapos3').offset().top - 
+    ($(window).height() / 1.2)){
+
+       
+
+        $('.diapos3 figure').each(function(i){
+
+            setTimeout(function(){
+
+            
+            $('.diapos3 figure').eq(i).addClass('is-showing');
+        }, 150 * (i+1));
+
+        });
+    }
+    
+
+
+
 
 });
